59254d111ecd2df0fa77472b9c2f2ac018a13a0e
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-02-19 18:09:24 -0500

    get email from params

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,36 @@
 import { getAttendeeFromEmail } from "@/server/server";
-export default function Home() {
-  const attendee = getAttendeeFromEmail("a3sharia@uwaterloo.ca");
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const email = searchParams.email as string;
+  if (!email) {
+    return (
+      <div className="grid place-items-center min-h-screen">
+        <div className="text-center space-y-4">
+          <h1 className="text-2xl font-bold">No email provided</h1>
+          <p className="text-gray-600">
+            Please provide an email in the URL parameters
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  const attendee = getAttendeeFromEmail(email);
+  if (!attendee) {
+    return (
+      <div className="grid place-items-center min-h-screen">
+        <div className="text-center space-y-4">
+          <h1 className="text-2xl font-bold">Attendee not found</h1>
+          <p className="text-gray-600">No attendee found with email: {email}</p>
+        </div>
+      </div>
+    );
+  }
+
   console.log(attendee);
 
   return (
