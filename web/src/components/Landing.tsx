49ba91ef58297a8ae0d4a705f2740e4f1baf353e
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-02-20 23:45:05 -0500

    spacing

--- web/src/components/Landing.tsx
+++ web/src/components/Landing.tsx
@@ -13,7 +13,7 @@ export const Landing = () => {
         <p className="text-sm text-slate-100 mt-2 font-medium">
           Socratica Symposium W25
         </p>
-        <p className="text-sm text-slate-400 mt-6">
+        <p className="text-sm text-slate-400 mt-1 text-center">
           {"If you're lost, enter the email that you used to RSVP"}
         </p>
         <input
@@ -21,7 +21,7 @@ export const Landing = () => {
           placeholder="eg. johndoe@uwaterloo.ca"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="mt-3 px-4 w-full md:min-w-96 py-3 rounded-full bg-white/5 text-white placeholder:text-white/40 border border-white/10 transition-all
+          className="mt-6 px-4 w-full md:min-w-96 py-3 rounded-full bg-white/5 text-white placeholder:text-white/40 border border-white/10 transition-all
       shadow-lg backdrop-blur-sm text-sm focus:outline-none focus:ring-2 focus:ring-white/80"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
